9e085c819d7a26b22f79b18497abaf3f429cb6d8
EdgarNovas <edgar.novas@enti.cat>
2025-10-25 12:30:53 +0200

    Diseño activity levels

--- Ahorcado/app/src/main/java/com/example/myapplication/MainActivity.kt
+++ Ahorcado/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.myapplication
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.MotionEvent
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -9,7 +11,17 @@ import androidx.core.view.WindowInsetsCompat
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         setContentView(R.layout.activity_main)
     }
+
+
+    override fun onTouchEvent(event: MotionEvent?): Boolean {
+        if(event?.action == MotionEvent.ACTION_DOWN){
+            val intent = Intent(this, SelectorNiveles::class.java)
+            startActivity(intent)
+            finish()
+        }
+        return super.onTouchEvent(event)
+    }
+
 }
